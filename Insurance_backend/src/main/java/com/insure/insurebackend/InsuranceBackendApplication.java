4703ff199da7255a64391ad89253f20a8342b24a
kollasumanth2357 <kollasumanth072@gmail.com>
2026-03-18 11:06:05 +0530

    Initial commit - Insure-AI full stack project

--- Insurance_backend/src/main/java/com/insure/insurebackend/InsuranceBackendApplication.java
+++ Insurance_backend/src/main/java/com/insure/insurebackend/InsuranceBackendApplication.java
@@ -4,10 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class InsuranceApplication {
+public class InsuranceBackendApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(InsuranceApplication.class, args);
+        SpringApplication.run(InsuranceBackendApplication.class, args);
     }
-
 }
